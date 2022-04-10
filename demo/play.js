730dfebc754a43d78039f95cbbe09e396198cc4d
Onno van der Zee <hastalavista@dds.nl>
2022-04-10 12:45:04 +0200

    store entries (demo)

--- demo/play.js
+++ demo/play.js
@@ -1,30 +1,63 @@
-const contents = Object.entries(transformCase('example'))
-const methods = contents.filter(pair => (typeof pair[1] === 'function'))
+const methods = Object.values(transformCase('example'))
+    .filter(value => typeof value === 'function')
 const delimiters = ' .:;,-+_/|\\&'.split('')
 
 window.onload = function () {
     const methodSelect = document.getElementById('method')
-    methods.forEach(method => {
-        methodSelect.innerHTML += `<option>${method[0]}</option>`
-    });
+    methodSelect.innerHTML = methods
+        .map(method => `<option>${method.name}</option>`)
+        .join('')
     const entryText = document.getElementById('entry')
     const yieldText = document.getElementById('yield')
 
     const delimit = document.getElementById('delimit')
     const preserve = document.getElementById('preserve')
     const delimitInput = document.getElementById('delimitInput')
-    delimiters.forEach(delimit => {
-        delimitInput.innerHTML += `<option value="${delimit}"
-            ${delimit === ' ' ? ' selected ' : ''}>
-            ${delimit === ' ' ? '(space)' : delimit}</option>`
-    });
+    delimitInput.innerHTML += delimiters
+        .map(delimiter => `<option value="${delimiter}"
+            ${delimiter === ' ' ? ' selected ' : ''}>
+            ${delimiter === ' ' ? '(space)' : delimiter}</option>`)
+        .join('')
     const delimitLetterNumber = document.getElementById('delimitLetterNumber')
     const delimitLowerUpper = document.getElementById('delimitLowerUpper')
     const delimitNumberLetter = document.getElementById('delimitNumberLetter')
     const delimitUpperLower = document.getElementById('delimitUpperLower')
     const delimitUpperUpperLower = document.getElementById('delimitUpperUpperLower')
 
-    getOptions = function () {
+    const displayStorage = function () {
+        const localStored = localStorage.getItem('transform-case-entries') || '[]'
+        const localStoredPhrases = JSON.parse(localStored)
+        localStore.innerHTML = localStoredPhrases.map(phrase => {
+            return `<label>${phrase}</label>`
+        }).join('')
+    }
+    const retrieveEntry = function (event) {
+        if (event.target.parentNode === this) {
+            entryText.value = event.target.innerText
+        }
+    }
+    const updateStorage = function () {
+        const localStored = localStorage.getItem('transform-case-entries') || '[]'
+        const localStoredSet = new Set(JSON.parse(localStored))
+        localStoredSet.add(entryText.value.trim())
+        const currentPhrases = JSON.stringify(Array.from(localStoredSet))
+        localStorage.setItem('transform-case-entries', currentPhrases)
+        displayStorage()
+    }
+    const resetStorage = function () {
+        localStorage.removeItem('transform-case-entries')
+        localStore.innerHTML = ''
+    }
+
+    const wordList = document.getElementById('words')
+    const localStore = document.getElementById('local-store')
+    localStore.addEventListener('click', retrieveEntry)
+    const clearButton = document.getElementById('clear-button')
+    clearButton.addEventListener('click', resetStorage)
+    const storeButton = document.getElementById('store-button')
+    storeButton.addEventListener('click', updateStorage)
+
+    const getOptions = function () {
         const options = {}
         options.delimit = delimit.value ? [delimit.value] : []
         options.preserve = preserve.value ? [preserve.value] : []
@@ -43,7 +76,13 @@ window.onload = function () {
         console.log('options', transformCase(entryText.value).options)
         console.log('phrase', transformCase(entryText.value)._phrase)
         console.log('words', transformCase(entryText.value).words)
+        console.log('method', methodSelect.value)
         yieldText.innerHTML = transformCase(entryText.value, getOptions())[methodSelect.value]()
+
+        wordList.innerHTML = transformCase(entryText.value)
+            .words
+            .map(word => `<li>${word}</li>`)
+            .join('')
     }
     entryText.oninput = function () {
         this.style.height = ''
@@ -59,5 +98,7 @@ window.onload = function () {
     delimitUpperLower.onchange = showTime
     delimitUpperUpperLower.onchange = showTime
     methodSelect.onchange = showTime
+
     showTime()
+    displayStorage()
 };
